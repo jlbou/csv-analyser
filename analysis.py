9f8c10a91b879c6873ceb7368a5921838b151cad
jlbou <joel.leon06@gmail.com>
2026-06-09 21:11:34 +0200

    Add charts module and update dataset to English

--- analysis.py
+++ analysis.py
@@ -1,12 +1,12 @@
 def get_summary(df):
-    '''Function to summarize the dataframe'''
+    """Function to summarize the dataframe"""
     summary = {'Column names': df.columns.to_list(),'Number of rows': df.shape[0], 'Number of columns': df.shape[1],
                'Number of missing values': df.isnull().sum().to_dict(), 'Number of duplicated': df.duplicated().sum(),
                'Statistics': df.describe()}
     return summary
 
 def get_column_info(df, column):
-    '''Function to get column information'''
+    """Function to get column information"""
     if df[column].dtype in ['int64', 'float64']:
         info = {'Data type': df[column].dtype,
                 'Unique values': df[column].nunique(),
